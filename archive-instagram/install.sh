c2184dbaf21f7bd28d997c2826166e7f17446452
Matt Crook <mattlcrook@icloud.com>
2019-10-12 16:02:53 +1300

    use absolute directories for systemctl link

--- archive-instagram/install.sh
+++ archive-instagram/install.sh
@@ -1,9 +1,11 @@
+DIR=dirname $(readlink -f "$0")
+
 pip3 install instaloader
 
 read -p "Your instagram username: " IG_USER
 instaloader --login=$IG_USER --sessionfile="$HOME/.automation/archive-instagram.session"
 
-systemctl link systemd/archive-instagram.service
-systemctl link systemd/archive-instagram.timer
+systemctl link $DIR/systemd/archive-instagram.service
+systemctl link $DIR/systemd/archive-instagram.timer
 
 systemctl enable archive-instagram.service
--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+DIR=dirname $(readlink -f "$0")
+
 mkdir -p "$HOME/.automation"
 
 loginctl enable-linger $USER
 
-systemctl link automation.slice
+systemctl link $DIR/automation.slice
 
 /bin/bash archive-instagram/install.sh
