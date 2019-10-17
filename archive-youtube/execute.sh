12b4c10001314eedfe5864a2696993e33a6f677a
Matt Crook <mattlcrook@icloud.com>
2019-10-18 00:02:50 +1300

    don't bother recoding youtube videos, too much processing

--- archive-youtube/execute.sh
+++ archive-youtube/execute.sh
@@ -13,4 +13,4 @@ if [ ! -r $PLAYLIST_DIR ]; then
   exit 1
 fi
 
-youtube-dl --add-metadata --no-overwrites --restrict-filenames --recode-video mp4 --output "%(playlist)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s" --ignore-errors --no-warnings --quiet --download-archive $ARCHIVE_FILE --batch-file $PLAYLIST_DIR
+youtube-dl --add-metadata --no-overwrites --restrict-filenames --output "%(playlist)s/%(playlist_index)s-%(title)s-%(id)s.%(ext)s" --ignore-errors --no-warnings --quiet --download-archive $ARCHIVE_FILE --batch-file $PLAYLIST_DIR
