abc81e61cc6736bab161290dc0b784ec81197ac0
Matt Crook <mattlcrook@icloud.com>
2019-10-21 06:48:49 +1300

    setup ig acc should be optional in install script

--- archive-instagram/install.sh
+++ archive-instagram/install.sh
@@ -2,8 +2,11 @@ DIR=$(dirname $(readlink -f "$0"))
 
 sudo pip3 install instaloader
 
-read -p "Your instagram username: " IG_USER
-instaloader --login=$IG_USER --sessionfile="$HOME/.automation/archive-instagram.session"
+read -p "Setup instagram account? [y/N] " C_SETUP_IG
+if [ $C_SETUP_IG = "y" ]; then
+  read -p "Your instagram username: " IG_USER
+  instaloader --login=$IG_USER --sessionfile="$HOME/.automation/archive-instagram.session"
+fi
 
 sudo systemctl link $DIR/systemd/archive-instagram.service
 
