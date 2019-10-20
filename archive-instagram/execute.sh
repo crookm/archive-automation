c5e9090f81cc6e4942393e65b101e0c35f14b645
Matt Crook <mattlcrook@icloud.com>
2019-10-21 06:50:25 +1300

    only download stories if signed in

--- archive-instagram/execute.sh
+++ archive-instagram/execute.sh
@@ -16,23 +16,23 @@ if [ ! -r $PROFILES_DIR ]; then
   exit 1
 fi
 
+# download posts
+instaloader $SESSION_ARGS --quiet --fast-update --no-captions --no-metadata-json --no-compress-json --no-video-thumbnails --dirname-pattern={profile} --filename-pattern={date_utc:%Y}/{date_utc}_UTC-{shortcode} +$PROFILES_DIR
+
 if [ -r $SESSION_DIR ]; then
   if [ ! -r $USERNAME_DIR ]; then
     echo >&2 "Could not access the username file: $USERNAME_DIR. This is required when specifying a session. Aborting."
     exit 1
   fi
-  
+
   SESSION_ARGS="--login=$(head -n 1 $USERNAME_DIR) --sessionfile=$SESSION_DIR"
 
   # download bookmarked posts
   instaloader $SESSION_ARGS --quiet --fast-update --no-caption --no-metadata-json --no-compress-json --no-video-thumbnails --dirname-pattern=__bookmarked --filename-pattern={date_utc:%Y}/{date_utc}_UTC-{profile}-{shortcode} ":saved"
+
+  # download stories
+  instaloader $SESSION_ARGS --quiet --fast-update --no-captions --no-metadata-json --no-compress-json --no-video-thumbnails --no-profile-pic --stories --no-posts --dirname-pattern={profile}/stories --filename-pattern={date_utc:%Y}/{date_utc}_UTC-{shortcode} +$PROFILES_DIR
+
+  # download highlights
+  instaloader $SESSION_ARGS --quiet --fast-update --no-captions --no-metadata-json --no-compress-json --no-video-thumbnails --no-profile-pic --highlights --no-posts --dirname-pattern={profile}/highlights --filename-pattern={date_utc:%Y}/{date_utc}_UTC-{shortcode} +$PROFILES_DIR
 fi
-
-# download posts
-instaloader $SESSION_ARGS --quiet --fast-update --no-captions --no-metadata-json --no-compress-json --no-video-thumbnails --dirname-pattern={profile} --filename-pattern={date_utc:%Y}/{date_utc}_UTC-{shortcode} +$PROFILES_DIR
-
-# download stories
-instaloader $SESSION_ARGS --quiet --fast-update --no-captions --no-metadata-json --no-compress-json --no-video-thumbnails --no-profile-pic --stories --no-posts --dirname-pattern={profile}/stories --filename-pattern={date_utc:%Y}/{date_utc}_UTC-{shortcode} +$PROFILES_DIR
-
-# download highlights
-instaloader $SESSION_ARGS --quiet --fast-update --no-captions --no-metadata-json --no-compress-json --no-video-thumbnails --no-profile-pic --highlights --no-posts --dirname-pattern={profile}/highlights --filename-pattern={date_utc:%Y}/{date_utc}_UTC-{shortcode} +$PROFILES_DIR
